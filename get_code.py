dd7773bb0dbfdf071bc1dd9d7f9dabeb9b86d1a3
BugaevAV <iustusvir@gmail.com>
2022-09-29 11:43:47 +0300

    changed an order of extracting the token from link(requred_token)

--- get_code.py
+++ get_code.py
@@ -2,21 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+import re
 
 
 def get_token_vk():
-    service = Service(executable_path=ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
-    driver.get("http://oauth.vk.com/authorize?client_id=51432598&scope=65536&redirect_uri=https://vk.com/im?media=&sel=-216114574&display=page&response_type=token")
-    WebDriverWait(driver, 240).until(ec.url_contains('https://api.vk.com/blank.html#access_token='))
+    service = Service(executable_path=EdgeChromiumDriverManager().install())
+    driver = webdriver.Edge(service=service)
+    driver.get("https://oauth.vk.com/authorize?client_id=8204781&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=stats,offline&response_type=token&v=5.131")
+    WebDriverWait(driver, 240).until(ec.url_contains('https://oauth.vk.com/blank.html#access_token='))
     link_of_token = driver.current_url
     driver.close()
-    required_token = link_of_token[43:-29]
-
+    required_token = re.sub(r"(^https://oauth.vk.com/blank.html#access_token=)([\w.-]*)(&.*)", r"\g<2>", link_of_token)
     return required_token
 
 
 if __name__ == '__main__':
-        get_token_vk()
+    print(get_token_vk())
