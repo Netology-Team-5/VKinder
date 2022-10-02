ac5b0c725d8d7c6a046e1fc5241745d3f7343feb
BugaevAV <iustusvir@gmail.com>
2022-10-02 21:20:36 +0300

    Revert "my local settings for the file get_code.py"
    This reverts commit f0e1f2c3cb02d768424096caa0598c312ad15fb7.

--- get_code.py
+++ get_code.py
@@ -2,13 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 import re
 
 
 def get_token_vk():
-    service = Service(executable_path=EdgeChromiumDriverManager().install())
-    driver = webdriver.Edge(service=service)
+    service = Service(executable_path=GeckoDriverManager().install())
+    driver = webdriver.Firefox(service=service)
     driver.get("https://oauth.vk.com/authorize?client_id=51432598&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=stats,offline&response_type=token&v=5.131")
     WebDriverWait(driver, 240).until(ec.url_contains('https://oauth.vk.com/blank.html#access_token='))
     link_of_token = driver.current_url
